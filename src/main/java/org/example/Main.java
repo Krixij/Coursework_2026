83d3fa646847b4e33a297084db5d90fa8da17fcb
krixij <Kutorov.ivan@mail.ru>
2026-03-04 17:27:40 +0500

    Demo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,27 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+    public static void main(String[] args) {
+        HttpClient client = HttpClient.newHttpClient();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+        // build a HttpRequest
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://ru.investing.com/currencies/usd-rub"))
+                .build();
+
+        // send asynchronous GET request and handle response.
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                // extract body as string
+                .thenApply(HttpResponse::body)
+                // retrieve extracted body
+                .thenAccept(htmlContent -> {
+                    System.out.println(htmlContent);
+                })
+
+                .join();
     }
 }
